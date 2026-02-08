41714c07706911be603fb103e015faec8feb3e2c
sunky0 <windo3o@hanmail.net>
2026-02-08 19:51:33 +0900

    thumnail

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,4 @@
 export const WEDDING_INVITATION_URL = 'https://stellaandhenry.github.io/wedding/';
-export const KAKAOTALK_API_TOKEN = '카카오톡 API 토큰을 입력해주세요!';
-export const KAKAOTALK_SHARE_IMAGE = '카카오톡으로 공유할 때 사용되는 대표이미지 URL을 입력해주세요!';
-
-export const WEDDING_DATE = '2026년 06월 06일, 오전 11시 00분';
-export const WEDDING_LOCATION = '○○○웨딩, ○층 ○○홀';
-
 export const ACCOUNT_INFO = [
     {
         type: '신랑',
